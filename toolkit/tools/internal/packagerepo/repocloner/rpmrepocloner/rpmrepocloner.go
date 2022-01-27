44c64070a231d2cc544a93eff24198ba1c0c4a52
MateuszMalisz <maliszmat@outlook.com>
2022-01-27 09:20:55 -0800

    Change destdir argument to downloaddir

--- toolkit/tools/internal/packagerepo/repocloner/rpmrepocloner/rpmrepocloner.go
+++ toolkit/tools/internal/packagerepo/repocloner/rpmrepocloner/rpmrepocloner.go
@@ -258,7 +258,7 @@ func (r *RpmRepoCloner) Clone(cloneDeps bool, packagesToClone ...*pkgjson.Packag
 
 		logger.Log.Debugf("Cloning: %s", pkgName)
 		args := []string{
-			"--destdir",
+			"--downloaddir",
 			chrootDownloadDir,
 			pkgName,
 		}
